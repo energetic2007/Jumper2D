e53fdab1d128064ebebae02d7f71a39d1bb7ef78
energetic 2007 <anna.pch.99@gmail.com>
2021-09-09 17:41:18 +0300

    add flying entity

---
+++ Assets/Scripts/FlyEntity.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyEntity : Entity
+{
+    private Vector3 dir;
+    public GameObject flyEntity;
+    private void Start()
+    {
+        dir = transform.right;
+    }
+    private void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        if (Mathf.Abs(transform.position.x) >= 2.5f)
+        {
+            Debug.Log("STOP");
+            dir *= -1f;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "DeathZone")
+        {
+            Die();
+        }
+    }
+}
