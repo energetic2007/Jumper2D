c390c9ff52d6fc7fcada7d2100328b7088f22949
energetic 2007 <anna.pch.99@gmail.com>
2022-04-03 11:42:36 +0300

    Fix platform

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform player;
-    private Vector3 pos;
+  [SerializeField] private Transform player;
+  private Vector3 pos;
 
-    private void Awake()
+  private void Awake()
+  {
+
+    if (!player)
+      player = FindObjectOfType<Player>().transform;
+
+  }
+  private void Update()
+  {
+    pos = player.position;
+    pos.z = -10.0f;
+    pos.x = 0;
+    pos.y -= 2;
+    if (pos.y > transform.position.y)
     {
-
-        if (!player)
-            player = FindObjectOfType<Player>().transform;
-
+      transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime + 0.8f);
     }
-    private void Update()
-    {
-        pos = player.position;
-        pos.z = -10.0f;
-        pos.y -= 2;
-        if (pos.y > transform.position.y)
-        {
-            transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime + 0.8f);
-        }
-    }
+  }
 }
 
